ca3e1dd56b68a18aac087233eb83a0def8a36749
TrungNguyen153 <nguyenviettrungdnvn@gmail.com>
2022-01-31 15:11:11 +0700

    add typescript helper

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -108,6 +108,7 @@ return require("packer").startup(function(use)
 
 	use({
 		"williamboman/nvim-lsp-installer",
+    requires = {"jose-elias-alvarez/nvim-lsp-ts-utils"}, -- for typescript helper
 		config = function()
 			require("lsp.lsp-installer")
 		end,
