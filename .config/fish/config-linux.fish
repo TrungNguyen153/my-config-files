4cd8a284855f2c050ceb2793b0bf967df957e610
TrungNguyen153 <nguyenviettrungdnvn@gmail.com>
2022-02-17 13:27:27 +0700

    Add flutter into lsp

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -7,6 +7,7 @@ set -gx PATH $ANDROID_HOME/tools/bin $PATH;
 set -gx PATH $ANDROID_HOME/platform-tools $PATH;
 set -gx PATH $ANDROID_HOME/emulator $PATH
 set -gx PATH $ANDROID_HOME/cmdline-tools/latest/bin $PATH
+set --export ADB $ANDROID_HOME/platform-tools/adb
 
 # JAVA_HOME
 set --export JAVA_HOME /usr/lib/jvm/java-11-openjdk-amd64
