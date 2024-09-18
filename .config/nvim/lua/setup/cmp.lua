cfccb30501316e44b4ec68e377590da2b4ceb74f
TrungNguyen153 <nguyenviettrungdnvn@gmail.com>
2024-09-18 23:13:01 +0700

    Fix cmp <Tab>

--- .config/nvim/lua/setup/cmp.lua
+++ .config/nvim/lua/setup/cmp.lua
@@ -222,7 +222,7 @@ return {
                 end),
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
-						cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
 					elseif luasnip.expand_or_locally_jumpable() then
 						luasnip.expand_or_jump()
 					-- elseif has_words_before() then
