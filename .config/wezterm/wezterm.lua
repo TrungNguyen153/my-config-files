a2658bf1194c1217d5f67baaa6832915729e8795
TrungNguyen <TrungNguyen153@gmail.com>
2026-05-04 23:50:03 +0700

    s

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,20 +4,44 @@ local is_linux = wezterm.target_triple:find('linux') ~= nil
 local is_windows = wezterm.target_triple:find('windows') ~= nil
 
 local mux = wezterm.mux
-local workspace_switcher = wezterm.plugin.require('https://github.com/MLFlexer/smart_workspace_switcher.wezterm')
-local session_manager = require('wezterm-session-manager/session-manager')
-wezterm.on('save_session', function(window)
-    session_manager.save_state(window)
-end)
-wezterm.on('load_session', function(window)
-    session_manager.load_state(window)
-end)
-wezterm.on('restore_session', function(window)
-    session_manager.restore_state(window)
-end)
 
 local function base_path_name(str)
     return string.gsub(str, '(.*[/\\])(.*)', '%2')
+end
+
+local function switch_workspace(window, pane)
+    local choices = { { id = '__new__', label = '+ New workspace...' } }
+    for _, name in ipairs(mux.get_workspace_names()) do
+        table.insert(choices, { id = name, label = name })
+    end
+    window:perform_action(
+        wezterm.action.InputSelector({
+            title = 'Switch workspace',
+            choices = choices,
+            fuzzy = true,
+            action = wezterm.action_callback(function(w, p, id, _)
+                if not id then
+                    return
+                end
+                if id == '__new__' then
+                    w:perform_action(
+                        wezterm.action.PromptInputLine({
+                            description = 'New workspace name:',
+                            action = wezterm.action_callback(function(w2, p2, line)
+                                if line and line ~= '' then
+                                    w2:perform_action(wezterm.action.SwitchToWorkspace({ name = line }), p2)
+                                end
+                            end),
+                        }),
+                        p
+                    )
+                else
+                    w:perform_action(wezterm.action.SwitchToWorkspace({ name = id }), p)
+                end
+            end),
+        }),
+        pane
+    )
 end
 
 local function update_right_status(window)
@@ -150,123 +174,11 @@ config.keys = {
     { key = 'x', mods = 'LEADER', action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
     -- Toggle full
     { key = 'n', mods = 'SHIFT|CTRL', action = 'ToggleFullScreen' },
-    -- Session Manager
-    { key = 'w', mods = 'LEADER', action = workspace_switcher.switch_workspace() },
-    { key = 'S', mods = 'LEADER', action = wezterm.action({ EmitEvent = 'save_session' }) },
-    { key = 'L', mods = 'LEADER', action = wezterm.action({ EmitEvent = 'load_session' }) },
-    { key = 'R', mods = 'LEADER', action = wezterm.action({ EmitEvent = 'restore_session' }) },
+    -- Workspace switcher
+    { key = 'w', mods = 'LEADER', action = wezterm.action_callback(switch_workspace) },
     -- Copy paste
     { key = 'V', mods = 'CTRL', action = wezterm.action.PasteFrom('Clipboard') },
     { key = 'C', mods = 'CTRL', action = wezterm.action.CopyTo('Clipboard') },
 }
 
 return config
-
--- local config = {
---   check_for_updates = false,
---   color_scheme = "GruvboxDarkHard",
---   inactive_pane_hsb = {
---     hue = 1.0,
---     saturation = 1.0,
---     brightness = 1.0,
---   },
---   default_prog = { "/bin/bash", "-l" },
---   font = wezterm.font("JetBrains Mono", { weight = "Regular" }),
---   font_size = 10.0,
---   launch_menu = {},
---   window_decorations = "INTEGRATED_BUTTONS|RESIZE",
---   hide_tab_bar_if_only_one_tab = true,
---   scrollback_lines = 3000,
---   enable_scroll_bar = false,
---   leader = { key = "t", mods = "CTRL" },
---   disable_default_key_bindings = true,
---   max_fps = 120,
---   keys = {
---     -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
---     { key = "a", mods = "LEADER|CTRL",  action = wezterm.action({ SendString = "\x01" }) },
---     -- split
---     {
---       key = "-",
---       mods = "LEADER",
---       action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
---     },
---     {
---       key = "\\",
---       mods = "LEADER",
---       action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
---     },
---     -- zoom focus pane
---     { key = "z", mods = "LEADER",       	action = "TogglePaneZoomState" },
---     -- switch pane
---     { key = "h", mods = "LEADER",       	action = wezterm.action({ ActivatePaneDirection = "Left" }) },
---     { key = "j", mods = "LEADER",       	action = wezterm.action({ ActivatePaneDirection = "Down" }) },
---     { key = "k", mods = "LEADER",       	action = wezterm.action({ ActivatePaneDirection = "Up" }) },
---     { key = "l", mods = "LEADER",       	action = wezterm.action({ ActivatePaneDirection = "Right" }) },
---     -- Adjust pane size
---     { key = "h", mods = "LEADER|CTRL",  	action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
---     { key = "j", mods = "LEADER|CTRL",  	action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
---     { key = "k", mods = "LEADER|CTRL",  	action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
---     { key = "l", mods = "LEADER|CTRL",  	action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
---     -- tab switching
---     { key = "1", mods = "LEADER",       	action = wezterm.action({ ActivateTab = 0 }) },
---     { key = "2", mods = "LEADER",       	action = wezterm.action({ ActivateTab = 1 }) },
---     { key = "3", mods = "LEADER",       	action = wezterm.action({ ActivateTab = 2 }) },
---     { key = "4", mods = "LEADER",       	action = wezterm.action({ ActivateTab = 3 }) },
---     { key = "5", mods = "LEADER",       	action = wezterm.action({ ActivateTab = 4 }) },
---     { key = "6", mods = "LEADER",       	action = wezterm.action({ ActivateTab = 5 }) },
---     { key = "7", mods = "LEADER",       	action = wezterm.action({ ActivateTab = 6 }) },
---     { key = "8", mods = "LEADER",       	action = wezterm.action({ ActivateTab = 7 }) },
---     { key = "9", mods = "LEADER",       	action = wezterm.action({ ActivateTab = 8 }) },
---     -- Open new tab
---     { key = "n", mods = "LEADER",       	action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
---     -- close Tab
---     { key = "&", mods = "LEADER|SHIFT", 	action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
---     -- close pane
---     { key = "x", mods = "LEADER",       	action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
---     -- Toggle full
---     { key = "n", mods = "SHIFT|CTRL",   	action = "ToggleFullScreen" },
---     -- Copy paste
---     { key = "v", mods = "SHIFT|CTRL",   	action = wezterm.action.PasteFrom("Clipboard") },
---     { key = "c", mods = "SHIFT|CTRL",   	action = wezterm.action.CopyTo("Clipboard") },
---     { key = "w", mods = "LEADER", 		action = workspace_switcher.switch_workspace(), },
---     -- Session Manager
---     {key  = "S",  mods = "LEADER|SHIFT", 	action = wezterm.action{EmitEvent = "save_session"}},
---     {key  = "L",  mods = "LEADER|SHIFT", 	action = wezterm.action{EmitEvent = "load_session"}},
---     {key  = "R",  mods = "LEADER|SHIFT", 	action = wezterm.action{EmitEvent = "restore_session"}},
---   },
---   set_environment_variables = {},
---   window_padding = {
---     left = '0cell',
---     right = '0cell',
---     top = '0.1cell',
---     bottom = '0cell',
---   },
--- }
-
--- if wezterm.target_triple == "x86_64-pc-windows-msvc" then
---   config.front_end = "OpenGL" -- OpenGL doesn't work quite well with RDP.
---   config.term = ""           -- Set to empty so FZF works on windows
---   config.default_prog = { "nu" }
---   config.window_background_image = ""
---   table.insert(config.launch_menu, { label = "PowerShell", args = { "powershell.exe", "-NoLogo" } })
---   table.insert(config.launch_menu, { label = "CMD", args = { "cmd.exe" } })
-
---   -- Find installed visual studio version(s) and add their compilation
---   -- environment command prompts to the menu
---   for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
---     local year = vsvers:gsub("Microsoft Visual Studio/", "")
---     table.insert(config.launch_menu, {
---       label = "x64 Native Tools VS " .. year,
---       args = {
---         "cmd.exe",
---         "/k",
---         "C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat",
---       },
---     })
---   end
--- else
---   table.insert(config.launch_menu, { label = "bash", args = { "bash", "-l" } })
---   table.insert(config.launch_menu, { label = "fish", args = { "fish", "-l" } })
--- end
-
--- return config
